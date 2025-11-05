2ea617ec7e77095bcabed627fcaa7ce10fd595ae
kkphoenix <kauaAlvesWorkplace@gmail.com>
2025-11-05 18:11:48 -0300

    init front-end

---
+++ front-end/src/app/app.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+
+  // { path: '', component: HomeComponent },
+  // { path: 'sobre', component: AboutComponent },
+
+  // { path: '**', component: NotFoundComponent },
+];

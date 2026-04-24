75578dc2e5479e3eb053369c2eeea1d60aa286a8
Kauã Alves Santos <kauaalvesworkplace@gmail.com>
2026-04-24 17:45:30 -0300

    just correcting upload path to inside backend.

--- backend/src/api/config/upload.ts
+++ backend/src/api/config/upload.ts
@@ -6,7 +6,7 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const uploadFolder = path.resolve(__dirname, '..', '..', '..', 'uploads');
+const uploadFolder = path.resolve(__dirname, '..', '..', 'uploads');
 
 export default {
   directory: uploadFolder,
